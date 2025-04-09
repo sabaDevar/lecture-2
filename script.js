9485b4910473f5a0419aa3cc2d6575d0580da2de
sabaDevar <saba.aptsiauri@outlook.com>
2025-04-09 22:02:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let clickMe = document.getElementById('clickMe')
+let label = document.getElementById('label')
+
+let count = 1
+
+clickMe.addEventListener('click', function() {
+    label.textContent =  count +=
+})
