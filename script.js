da5dcb97495f97e6c59d239cda303f25b4a1985a
sabaDevar <saba.aptsiauri@outlook.com>
2025-04-11 13:52:50 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let clickMe = document.getElementById('clickMe')
-let label = document.getElementById('label')
-
-let count = 1
-
-clickMe.addEventListener('click', function() {
-    label.textContent =  count +=
-})
